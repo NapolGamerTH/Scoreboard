be7efb66f3dd0bd241e3fe4f2ef3d52489bf97b8
NapolGamer TH <knightnapol15@gmail.com>
2018-10-27 09:08:15 +0700

    Update ScoreboardTask.php
    The biggest update !!

--- src/Napol/Scoreboard/Task/ScoreboardTask.php
+++ src/Napol/Scoreboard/Task/ScoreboardTask.php
@@ -24,49 +24,49 @@ class ScoreboardTask extends Task {
 		if(!isset($titles[$this->titleIndex])) $this->titleIndex = 0;
 		$api = Scoreboard::getInstance();
 		foreach($this->plugin->getServer()->getOnlinePlayers() as $p){
-    /* <------------------------------> */
-      $player = $p->getPlayer();
-      $name = $player->getName();
-      $online = $online = count(Server::getInstance()->getOnlinePlayers());
-      $tps = $this->getServer()->getTicksPerSecond();
-      $usage = $this->getServer()->getTickUsage();
-      $max_online = $this->getServer()->getMaxPlayers();
-      $fac = $this->plugin->getServer()-getPluginManager()->getPlugin("FactionsPro")->getPlayerFaction($player->getName());
-      $x = round($player->getX(), 0);
-      $y = round($player->getY(), 0);
-      $z = round($player->getZ(), 0);
-      $group = $this->plugin->getServer()-getPluginManager()->getPlugin("PurePerms")->getUserDataMgr()->getGroup($player)->getName();
-      $money = $this->plugin->getServer()-getPluginManager()->getPlugin("EconomyAPI")->myMoney($player->getName());
-      $item = $player->getInventory()->getItemInHand()->getName();
-      $id = $player->getInventory()->getItemInHand()->getId();
-      $ids = $player->getInventory()->getItemInHand()->getDamage();
-      $level = $player->getLevel()->getName();
-      $date = date("H.i");
-      $kills = $this->plugin->getServer()-getPluginManager()->getPlugin("KillChat")->getKills($name);
-      $deaths = $this->plugin->getServer()-getPluginManager()->getPlugin("KillChat")->getDeaths($name);
-      $ping = $player->getPing($name);
-      /* str_replace */
-      $lines = $config->get("line");
-      $lines = str_replace("{name}", $name, $lines);
-      $lines = str_replace("{level}", $level, $lines);
-      $lines = str_replace("{time}", $date, $lines);
-      $lines = str_replace("{id}", $id, $lines);
-      $lines = str_replace("{ids}", $ids, $lines);
-      $lines = str_replace("{item}", $item, $lines);
-      $lines = str_replace("{group}", $group, $lines);
-      $lines = str_replace("{faction}", $fac, $lines);
-      $lines = str_replace("{max_online}", $max_online, $lines);
-      $lines = str_replace("{online}", $online, $lines);
-      $lines = str_replace("{tps}", $tps, $lines);
-      $lines = str_replace("{usage}", $usage, $lines);
-      $lines = str_replace("{kills}", $kills, $lines);
-      $lines = str_replace("{x}", $x, $lines);
-      $lines = str_replace("{y}", $y, $lines);
-      $lines = str_replace("{z}", $z, $lines);
-      $lines = str_replace("{money}", $money, $lines);
-      $lines = str_replace("{deaths}", $deaths, $lines);
-      $lines = str_replace("{ping}", $ping, $lines);
-      /* <------------------------------> */
+                /* <------------------------------> */
+                        $player = $p->getPlayer();
+                        $name = $player->getName();
+                        $tps = $this->getServer()->getTicksPerSecond();
+                        $usage = $this->getServer()->getTickUsage();
+			$online = $online = count(Server::getInstance()->getOnlinePlayers());
+                        $max_online = $this->getServer()->getMaxPlayers();
+                        $fac = $this->plugin->getServer()-getPluginManager()->getPlugin("FactionsPro")->getPlayerFaction($player->getName());
+                        $x = round($player->getX(), 0);
+                        $y = round($player->getY(), 0);
+                        $z = round($player->getZ(), 0);
+                        $group = $this->plugin->getServer()-getPluginManager()->getPlugin("PurePerms")->getUserDataMgr()->getGroup($player)->getName();
+                        $money = $this->plugin->getServer()-getPluginManager()->getPlugin("EconomyAPI")->myMoney($player->getName());
+                        $item = $player->getInventory()->getItemInHand()->getName();
+                        $id = $player->getInventory()->getItemInHand()->getId();
+                        $ids = $player->getInventory()->getItemInHand()->getDamage();
+                        $level = $player->getLevel()->getName();
+                        $date = date("H.i");
+                        $kills = $this->plugin->getServer()-getPluginManager()->getPlugin("KillChat")->getKills($name);
+                        $deaths = $this->plugin->getServer()-getPluginManager()->getPlugin("KillChat")->getDeaths($name);
+                        $ping = $player->getPing($name);
+                /* str_replace */
+                        $lines = $config->get("line");
+                        $lines = str_replace("{name}", $name, $lines);
+                        $lines = str_replace("{tps}", $tps, $lines);
+                        $lines = str_replace("{usage}", $usage, $lines);
+                        $lines = str_replace("{online}", $online, $lines);
+                        $lines = str_replace("{max_online}", $ids, $lines);
+                        $lines = str_replace("{faction}", $fac, $lines);
+                        $lines = str_replace("{x}", $x, $lines);
+                        $lines = str_replace("{y}", $y, $lines);
+                        $lines = str_replace("{z}", $z, $lines);
+                        $lines = str_replace("{group}", $group, $lines);
+                        $lines = str_replace("{money}", $money, $lines);
+                        $lines = str_replace("{item}", $item, $lines);
+                        $lines = str_replace("{id}", $id, $lines);
+                        $lines = str_replace("{ids}", $ids, $lines);
+                        $lines = str_replace("{level}", $level, $lines);
+                        $lines = str_replace("{time}", $date, $lines);
+			$lines = str_replace("{kills}", $kills, $lines);
+                        $lines = str_replace("{deaths}", $deaths, $lines);
+                        $lines = str_replace("{ping}", $ping, $lines);
+                /* <------------------------------> */
 			$api->new($p, $p->getName(), $titles[$this->titleIndex]);
 			$i = 0;
 			foreach($lines as $line){
